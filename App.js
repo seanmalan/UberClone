d7ffeac275497cc72b6e955b27bd6d4f59ce72fe
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-07-12 17:19:31 +1200

    have added the first two nav options for ride and food

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./Store";
+import HomeScreen from "./HomeScreen";
+import tw from "tailwind-react-native-classnames";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+// 1) set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/NavOptions.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { FlatList } from "react-native";
+import { TouchableOpacity } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen", // add this later
+  },
+
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen", // add this later
+  },
+];
+
+const NavOptions = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
