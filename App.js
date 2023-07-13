6e28a3d5f7d2888b22cf7c8797c2f6b1d9853e76
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-07-13 12:36:54 +1200

    removes screens from component folder

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./Store";
-import HomeScreen from "./components/screens/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 import tw from "tailwind-react-native-classnames";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import MapScreen from "./components/screens/MapScreen";
+import MapScreen from "./screens/MapScreen";
 
 // 1) set up redux
 
@@ -18,10 +18,16 @@ export default function App() {
       <NavigationContainer>
         <SafeAreaProvider>
           <Stack.Navigator>
-            <Stack.Screen name="HomeScreen" component={HomeScreen} 
-            options={{headerShown: false,}}/>
-            <Stack.Screen name="MapScreen" component={MapScreen} 
-            options={{headerShown: false,}}/>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
           </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
