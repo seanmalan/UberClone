f2db6ff8c805a8e529ea012fcc6c4cc138131dcd
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-07-13 12:33:43 +1200

    moves HomeScreen to the screen folder. created the MapScreen

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { Image } from "react-native";
-import NavOptions from "./components/NavOptions";
+import NavOptions from "../NavOptions";
 
 const HomeScreen = () => {
   return (
