e9c377e2534cda77fdca57200a2582d692e74b45
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-07-13 12:43:58 +1200

    adds the dotenv package

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    Plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+    ],
   };
 };
