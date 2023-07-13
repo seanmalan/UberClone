c77fb91dd23bb25a0d7366539ee84a9c27b3d3e2
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-07-13 12:35:12 +1200

    adds stack navigator and stack screen

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import { Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./Store";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./components/screens/HomeScreen";
 import tw from "tailwind-react-native-classnames";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MapScreen from "./components/screens/MapScreen";
 
 // 1) set up redux
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} 
+            options={{headerShown: false,}}/>
+            <Stack.Screen name="MapScreen" component={MapScreen} 
+            options={{headerShown: false,}}/>
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
